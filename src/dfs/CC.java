c6b11a8ad4ed29b24c400a3701d4bd527ce92bb8
Yang <49736123+s651794071@users.noreply.github.com>
2022-04-28 23:03:02 +0800

    把visited数组改成整数数组，用于新增方法isConnected判断两个顶点是否是连通的

--- src/dfs/CC.java
+++ src/dfs/CC.java
@@ -4,26 +4,29 @@ package dfs;
 public class CC {
 
     private Graph G;
-    private boolean[] visited;
+    private int[] visited;
     private int cccount = 0; // connected components count
 
     public CC(Graph G) {
         this.G = G;
-        visited = new boolean[G.getV()];
+        visited = new int[G.getV()];
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = -1;
+        }
         for (int v = 0; v < G.getV(); v++) {
-            if(!visited[v]){
-                dfs(v);
+            if(visited[v] == -1){
+                dfs(v, cccount);
                 cccount++;
             }
         }
     }
 
-    private void dfs(int v) {
+    private void dfs(int v, int ccid) {
 
-        visited[v] = true;
+        visited[v] = ccid;
         for(int w: G.adj(v)) {
-            if(!visited[w])
-                dfs(w);
+            if(visited[w] == -1)
+                dfs(w, ccid);
         }
     }
 
@@ -31,9 +34,14 @@ public class CC {
         return cccount;
     }
 
+    public boolean isConnected(int v, int w) {
+        return visited[v] == visited[w];
+    }
+
     public static void main(String[] args) {
         Graph g = new Graph("g.txt");
         CC cc = new CC(g);
         System.out.println(cc.count());
+        System.out.println(cc.isConnected(0,5));
     }
 }
