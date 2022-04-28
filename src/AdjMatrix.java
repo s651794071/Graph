19d562b654ce2767d6f6c8a9ca7a80f87b5a40a9
Yang <49736123+s651794071@users.noreply.github.com>
2022-04-28 18:15:37 +0800

    对传入文件中的vertex和edge的值进行合法性判断

--- src/AdjMatrix.java
+++ src/AdjMatrix.java
@@ -12,14 +12,21 @@ public class AdjMatrix {
         try (Scanner scanner = new Scanner(file)) {
 
             V = scanner.nextInt();
-
+            if (V < 0)
+                throw new IllegalArgumentException("V must be non-negative!");
             adj = new int[V][V];
 
             E = scanner.nextInt();
-
+            if (E < 0)
+                throw new IllegalArgumentException("E must be non-negative!");
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                validateVertex(a); // 判断顶点a是否有效
                 int b = scanner.nextInt();
+                validateVertex(b); // 判断顶点b是否有效
+
+                if (a == b) throw new IllegalArgumentException("我们只考虑简单图，不考虑自环边！");
+                if (adj[a][b] == 1) throw new IllegalArgumentException("我们只考虑简单图，不考虑平行边！");
 
                 adj[a][b] = 1;
                 adj[b][a] = 1;
@@ -27,6 +34,15 @@ public class AdjMatrix {
         }catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 测试顶点v是否是有效的，假如 V = 5 ，但文件中某个 v 大于等于了5 （顶点序号从0开始） 就是不合法的
+     * @param v 某个顶点的序号
+     */
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is invalid");
     }
 
     @Override
