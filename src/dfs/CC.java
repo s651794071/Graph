f106141340e63567fb1597baebacfe2d7a98b48b
Yang <49736123+s651794071@users.noreply.github.com>
2022-04-28 23:18:15 +0800

    新增方法components()返回图中所有的连通分量，非常好用！

--- src/dfs/CC.java
+++ src/dfs/CC.java
@@ -1,5 +1,7 @@
 package dfs;
 
+import java.util.ArrayList;
+
 // Connected Component
 public class CC {
 
@@ -38,10 +40,30 @@ public class CC {
         return visited[v] == visited[w];
     }
 
+    // 返回图中所有的连通分量
+    public ArrayList<Integer>[] components(){
+        ArrayList<Integer>[] res = new ArrayList[cccount];
+        for (int i = 0; i < cccount; i++)
+            res[i] = new ArrayList<>();
+
+        for (int v = 0; v < G.getV(); v++)
+            res[visited[v]].add(v); // visited数组中v的范围在[0, ccount-1]
+
+        return res;
+    }
+
     public static void main(String[] args) {
         Graph g = new Graph("g.txt");
         CC cc = new CC(g);
         System.out.println(cc.count());
         System.out.println(cc.isConnected(0,5));
+        System.out.println("----------");
+        ArrayList<Integer>[] comp = cc.components();
+        for (int ccid = 0; ccid < comp.length; ccid++) {
+            System.out.print(ccid + ": ");
+            for (int w: comp[ccid])
+                System.out.print(w + " ");
+            System.out.println();
+        }
     }
 }
